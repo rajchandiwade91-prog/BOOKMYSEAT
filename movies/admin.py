98cc736fa4759a316c46cab06e6e15fc2de75f68
raj chandiwade91-prog <rajchandiwade91@gmail.com>
2026-04-26 18:34:28 +0530

    Clean project structure, fix settings.py template path, add movies.json fixture, update templates

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Movie, Theater, Seat,Booking
+from .models import Movie, Theater, Seat, Booking
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'cast','description']
+    list_display = ['name', 'rating', 'cast', 'description']
 
 @admin.register(Theater)
 class TheaterAdmin(admin.ModelAdmin):
@@ -15,4 +15,4 @@ class SeatAdmin(admin.ModelAdmin):
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ['user', 'seat', 'movie','theater','booked_at']
+    list_display = ['user', 'movie', 'theater', 'seat', 'booked_at']
